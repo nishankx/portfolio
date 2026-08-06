535fe7410f068430bc0363d73faff59f8dc28656
nishankx <nishankashyapofficial@gmail.com>
2026-08-07 02:09:46 +0530

    fixing mobile responsivity #02

--- src/components/portfolioComponents/PageSections/About.jsx
+++ src/components/portfolioComponents/PageSections/About.jsx
@@ -207,7 +207,7 @@ const About = () => {
             </a>
           </div>
 
-          <div className='flex mx-2 gap-2 my-3 items-center'>
+          <div className='flex flex-wrap mx-2 gap-2 my-3 items-center'>
 
             <a href='https://instagram.com/nishankxp' target="_blank">
               <FaInstagramSquare className='backdrop-blur-xl bg-neutral-600/100 px-2 rounded-md text-5xl hover:bg-white/20 hover:scale-110 transition text-white'/>
@@ -224,7 +224,7 @@ const About = () => {
             <a
               href='/assets/docs/RESUME.pdf'
               download
-              className='flex backdrop-blur-xl ml-1 bg-neutral-600/100 items-center px-2 rounded-md gap-1 text-white hover:bg-white/20 hover:scale-110 transition'>
+              className='flex w-fit backdrop-blur-xl ml-1 bg-neutral-600/100 items-center px-2 rounded-md gap-1 text-white hover:bg-white/20 hover:scale-110 transition'>
 
               <FaCloudDownloadAlt className='text-5xl'/>
 
